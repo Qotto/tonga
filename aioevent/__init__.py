b4ad91fcef853005907e56b5f5ce9ae133831bc5
danglat <theo.dangla@epitech.eu>
2019-05-20 15:55:27 +0200

    Load from full patch

--- aioevent/__init__.py
+++ aioevent/__init__.py
@@ -2,10 +2,10 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from .app.aioevent import AioEvent
-from .model.result.result import BaseResult
-from .model.command.command import BaseCommand
-from .model.event.event import BaseEvent
+from aioevent.app.aioevent import AioEvent
+from aioevent.model.result.result import BaseResult
+from aioevent.model.command.command import BaseCommand
+from aioevent.model.event.event import BaseEvent
 
 __all__ = [
     'AioEvent',
