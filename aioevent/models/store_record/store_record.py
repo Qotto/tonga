e872595fb705aa6bb5d6c91af73f3c865e0bfaf8
danglat <theo.dangla@epitech.eu>
2019-05-22 16:08:43 +0200

    Remove key / type / value from StoreRecord attributes

--- aioevent/models/store_record/store_record.py
+++ aioevent/models/store_record/store_record.py
@@ -5,21 +5,15 @@
 from typing import Dict, Any
 
 from aioevent.models.store_record.base import BaseStoreRecord
+
 __all__ = [
     'StoreRecord'
 ]
 
 
 class StoreRecord(BaseStoreRecord):
-    key: str
-    type: str
-    value: bytes
-
-    def __init__(self, key: str, ttype: str, value: bytes, **kwargs) -> None:
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.key = key
-        self.type = ttype
-        self.value = value
 
     @classmethod
     def from_data(cls, event_data: Dict[str, Any]):
