6dbff5b3179d118a272d5636f0250df998a359ca
danglat <theo.dangla@epitech.eu>
2019-05-22 15:42:52 +0200

    Update BaseHandler (better decoupling)

--- aioevent/models/handler/base.py
+++ aioevent/models/handler/base.py
@@ -2,17 +2,8 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from aiokafka import TopicPartition
-
-from typing import Union
-
-from aioevent.models.events.base import BaseModel
-
 
 class BaseHandler:
     @classmethod
     def handler_name(cls) -> str:
-        raise NotImplementedError
-
-    async def handle(self, event: BaseModel, tp: TopicPartition, group_id: str, offset: int) -> Union[str, None]:
         raise NotImplementedError
