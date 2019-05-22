bc96d0838b16a15e44ce9f4d6182e31f55dd86eb
danglat <theo.dangla@epitech.eu>
2019-05-22 15:47:21 +0200

    Make tests async

--- tests/test_global_memory_stores.py
+++ tests/test_global_memory_stores.py
@@ -11,111 +11,127 @@ from aioevent.models.exceptions import StoreMetadataCantNotUpdated, StoreKeyNotF
 
 
 # Initialization test
-def test_global_memory_store_set_store_position(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_set_store_position(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 0}
     current_instance = 2
     nb_replica = 4
-    global_memory_store.set_store_position(current_instance, nb_replica, assigned_partitions, last_offsets)
+    await global_memory_store.set_store_position(current_instance, nb_replica, assigned_partitions, last_offsets)
 
     db_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica)
-    assert global_memory_store.get_metadata().__dict__ == db_meta.__dict__
+    r_db_meta = await global_memory_store.get_metadata()
+    assert r_db_meta.to_dict() == db_meta.to_dict()
 
 
 # Test raise UninitializedStore
-def test_global_memory_uninitialized_store(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_uninitialized_store(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(UninitializedStore):
-        global_memory_store.get('test')
+        await global_memory_store.get('test')
     with pytest.raises(UninitializedStore):
-        global_memory_store.get_all()
+        await global_memory_store.get_all()
 
 
 # Test build store
-def test_local_memory_store_build_set(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_build_set(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
-    global_memory_store.global_set('test', b'value')
-    global_memory_store.global_set('test2', b'value')
-    global_memory_store.global_set('test3', b'value')
-    global_memory_store.global_delete('test2')
+    await global_memory_store.global_set('test', b'value')
+    await global_memory_store.global_set('test2', b'value')
+    await global_memory_store.global_set('test3', b'value')
+    await global_memory_store.global_delete('test2')
 
     global_memory_store.set_initialized(True)
     assert global_memory_store.is_initialized
-    assert global_memory_store.get('test') == b'value'
+    assert await global_memory_store.get('test') == b'value'
     with pytest.raises(StoreKeyNotFound):
-        global_memory_store.get('test2')
-    assert global_memory_store.get('test3') == b'value'
-    global_memory_store.global_delete('test')
-    global_memory_store.global_delete('test3')
+        await global_memory_store.get('test2')
+    assert await global_memory_store.get('test3') == b'value'
+    await global_memory_store.global_delete('test')
+    with pytest.raises(StoreKeyNotFound):
+        await global_memory_store.get('test')
+    await global_memory_store.global_delete('test3')
+    with pytest.raises(StoreKeyNotFound):
+        await global_memory_store.get('test3')
 
 
 # Test set function
-def test_global_memory_store__global_set(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store__global_set(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
-    global_memory_store.global_set('test', b'value')
-    assert global_memory_store.get('test') == b'value'
+    await global_memory_store.global_set('test', b'value')
+    assert await global_memory_store.get('test') == b'value'
 
 
-def test_global_memory_store_set_bad_key(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_set_bad_key(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(ValueError):
-        assert global_memory_store.global_set(b'test', b'value')
+        assert await global_memory_store.global_set(b'test', b'value')
 
 
-def test_global_memory_store_global_set_metadata(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_global_set_metadata(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(StoreMetadataCantNotUpdated):
-        global_memory_store.global_set('metadata', b'value')
+        await global_memory_store.global_set('metadata', b'value')
 
 # Test get function
 
 
-def test_global_memory_store_get(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_get(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
-    assert global_memory_store.get('test') == b'value'
+    assert await global_memory_store.get('test') == b'value'
 
 
-def test_global_memory_store_get_not_found(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_get_not_found(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(StoreKeyNotFound):
-        global_memory_store.get('toto')
+        await global_memory_store.get('toto')
 
 
-def test_global_memory_store_get_bad_key(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_get_bad_key(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(ValueError):
-        global_memory_store.get(b'toto')
+        await global_memory_store.get(b'toto')
+
 
 # Test delete function
-
-
-def test_global_memory_store_global_delete_not_found(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_global_delete_not_found(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(StoreKeyNotFound):
-        global_memory_store.global_delete('toto')
+        await global_memory_store.global_delete('toto')
 
 
-def test_global_memory_store_global_delete_bad_key(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_global_delete_bad_key(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     with pytest.raises(ValueError):
-        global_memory_store.global_delete(b'toto')
+        await global_memory_store.global_delete(b'toto')
 
 
-def test_global_memory_store_global_delete(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_global_delete(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
-    global_memory_store.global_delete('test')
+    await global_memory_store.global_delete('test')
     with pytest.raises(StoreKeyNotFound):
-        assert global_memory_store.get('test')
+        await global_memory_store.get('test')
+
 
 # Test get all
-
-
-def test_global_memory_store_get_all(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_store_get_all(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
-    global_memory_store.global_set('test1', b'value1')
-    global_memory_store.global_set('test2', b'value2')
+    await global_memory_store.global_set('test1', b'value1')
+    await global_memory_store.global_set('test2', b'value2')
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 0}
@@ -123,13 +139,13 @@ def test_global_memory_store_get_all(get_global_memory_store_connection):
     nb_replica = 4
     meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica)
 
-    assert global_memory_store.get_all() == {'test1': b'value1', 'test2': b'value2',
-                                             'metadata': bytes(str(meta.to_dict()), 'utf-8')}
+    assert await global_memory_store.get_all() == {'test1': b'value1', 'test2': b'value2',
+                                                   'metadata': bytes(str(meta.to_dict()), 'utf-8')}
+
 
 # Test update_metadata_tp_offset function
-
-
-def test_global_memory_get_metadata(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_get_metadata(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
 
     assigned_partitions = [TopicPartition('test', 2)]
@@ -138,14 +154,15 @@ def test_global_memory_get_metadata(get_global_memory_store_connection):
     nb_replica = 4
     local_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica).to_dict()
 
-    db_meta = global_memory_store.get_metadata().to_dict()
-    assert db_meta == local_meta
+    db_meta = await global_memory_store.get_metadata()
+    assert db_meta.to_dict() == local_meta
 
 
-def test_global_memory_update_metadata_tp_offset(get_global_memory_store_connection):
+@pytest.mark.asyncio
+async def test_global_memory_update_metadata_tp_offset(get_global_memory_store_connection):
     global_memory_store = get_global_memory_store_connection
     tp = TopicPartition('test', 2)
-    global_memory_store.update_metadata_tp_offset(tp, 4)
+    await global_memory_store.update_metadata_tp_offset(tp, 4)
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 4}
@@ -153,5 +170,5 @@ def test_global_memory_update_metadata_tp_offset(get_global_memory_store_connect
     nb_replica = 4
     local_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica).to_dict()
 
-    db_meta = global_memory_store.get_metadata().to_dict()
-    assert db_meta == local_meta
+    db_meta = await global_memory_store.get_metadata()
+    assert db_meta.to_dict() == local_meta
--- tests/test_local_memory_stores.py
+++ tests/test_local_memory_stores.py
@@ -11,112 +11,126 @@ from aioevent.models.exceptions import StoreMetadataCantNotUpdated, StoreKeyNotF
 
 
 # Initialization test
-def test_local_memory_store_set_store_position(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_set_store_position(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 0}
     current_instance = 2
     nb_replica = 4
-    local_memory_store.set_store_position(current_instance, nb_replica, assigned_partitions, last_offsets)
+    await local_memory_store.set_store_position(current_instance, nb_replica, assigned_partitions, last_offsets)
 
     db_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica)
-    assert local_memory_store.get_metadata().__dict__ == db_meta.__dict__
+    r_db_meta = await local_memory_store.get_metadata()
+    assert r_db_meta.to_dict() == db_meta.to_dict()
 
 
 # Test raise UninitializedStore
-def test_local_memory_uninitialized_store(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_uninitialized_store(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(UninitializedStore):
-        local_memory_store.set('test', b'value')
+        await local_memory_store.set('test', b'value')
     with pytest.raises(UninitializedStore):
-        local_memory_store.get('test')
+        await local_memory_store.get('test')
     with pytest.raises(UninitializedStore):
-        local_memory_store.delete('test')
+        await local_memory_store.delete('test')
     with pytest.raises(UninitializedStore):
-        local_memory_store.get_all()
+        await local_memory_store.get_all()
 
 
 # Test build store
-def test_local_memory_store_build_set(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_build_set(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
-    local_memory_store.build_set('test', b'value')
-    local_memory_store.build_set('test2', b'value')
-    local_memory_store.build_set('test3', b'value')
-    local_memory_store.build_delete('test2')
+    await local_memory_store.build_set('test', b'value')
+    await local_memory_store.build_set('test2', b'value')
+    await local_memory_store.build_set('test3', b'value')
+    await local_memory_store.build_delete('test2')
 
     local_memory_store.set_initialized(True)
     assert local_memory_store.is_initialized
-    assert local_memory_store.get('test') == b'value'
+    assert await local_memory_store.get('test') == b'value'
     with pytest.raises(StoreKeyNotFound):
-        local_memory_store.get('test2')
-    assert local_memory_store.get('test3') == b'value'
-    local_memory_store.delete('test')
-    local_memory_store.delete('test3')
+        await local_memory_store.get('test2')
+    assert await local_memory_store.get('test3') == b'value'
+    await local_memory_store.delete('test')
+    await local_memory_store.delete('test3')
 
 
 # Test set function
-def test_local_memory_store_set(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_set(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
-    local_memory_store.set('test', b'value')
-    assert local_memory_store.get('test') == b'value'
+    await local_memory_store.set('test', b'value')
+    assert await local_memory_store.get('test') == b'value'
 
 
-def test_local_memory_store_set_bad_key(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_set_bad_key(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(ValueError):
-        assert local_memory_store.set(b'test', b'value')
+        assert await local_memory_store.set(b'test', b'value')
 
 
-def test_local_memory_store_set_metadata(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_set_metadata(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(StoreMetadataCantNotUpdated):
-        local_memory_store.set('metadata', b'value')
+        await local_memory_store.set('metadata', b'value')
 
 
 # Test get function
-def test_local_memory_store_get(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_get(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
-    assert local_memory_store.get('test') == b'value'
+    assert await local_memory_store.get('test') == b'value'
 
 
-def test_local_memory_store_get_not_found(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_get_not_found(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(StoreKeyNotFound):
-        local_memory_store.get('toto')
+        await local_memory_store.get('toto')
 
 
-def test_local_memory_store_get_bad_key(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_get_bad_key(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(ValueError):
-        local_memory_store.get(b'toto')
+        await local_memory_store.get(b'toto')
 
 
 # Test delete function
-def test_local_memory_store_delete_not_found(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_delete_not_found(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(StoreKeyNotFound):
-        local_memory_store.delete('toto')
+        await local_memory_store.delete('toto')
 
 
-def test_local_memory_store_delete_bad_key(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_delete_bad_key(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     with pytest.raises(ValueError):
-        local_memory_store.delete(b'toto')
+        await local_memory_store.delete(b'toto')
 
 
-def test_local_memory_store_delete(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_delete(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
-    local_memory_store.delete('test')
+    await local_memory_store.delete('test')
     with pytest.raises(StoreKeyNotFound):
-        assert local_memory_store.get('test')
+        assert await local_memory_store.get('test')
 
 
 # Test get all
-def test_local_memory_store_get_all(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_store_get_all(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
-    local_memory_store.set('test1', b'value1')
-    local_memory_store.set('test2', b'value2')
+    await local_memory_store.set('test1', b'value1')
+    await local_memory_store.set('test2', b'value2')
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 0}
@@ -124,26 +138,28 @@ def test_local_memory_store_get_all(get_local_memory_store_connection):
     nb_replica = 4
     meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica)
 
-    assert local_memory_store.get_all() == {'test1': b'value1', 'test2': b'value2',
-                                            'metadata': bytes(str(meta.to_dict()), 'utf-8')}
+    assert await local_memory_store.get_all() == {'test1': b'value1', 'test2': b'value2',
+                                                  'metadata': bytes(str(meta.to_dict()), 'utf-8')}
 
 
 # Test update_metadata_tp_offset function
-def test_local_memory_get_metadata(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_get_metadata(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 0}
     current_instance = 2
     nb_replica = 4
-    db_meta = local_memory_store.get_metadata().to_dict()
+    db_meta = await local_memory_store.get_metadata()
     local_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica).to_dict()
-    assert db_meta == local_meta
+    assert db_meta.to_dict() == local_meta
 
 
-def test_local_memory_update_metadata_tp_offset(get_local_memory_store_connection):
+@pytest.mark.asyncio
+async def test_local_memory_update_metadata_tp_offset(get_local_memory_store_connection):
     local_memory_store = get_local_memory_store_connection
     tp = TopicPartition('test', 2)
-    local_memory_store.update_metadata_tp_offset(tp, 4)
+    await local_memory_store.update_metadata_tp_offset(tp, 4)
 
     assigned_partitions = [TopicPartition('test', 2)]
     last_offsets = {TopicPartition('test', 2): 4}
@@ -151,5 +167,5 @@ def test_local_memory_update_metadata_tp_offset(get_local_memory_store_connectio
     nb_replica = 4
     local_meta = BaseStoreMetaData(assigned_partitions, last_offsets, current_instance, nb_replica).to_dict()
 
-    db_meta = local_memory_store.get_metadata().to_dict()
-    assert db_meta == local_meta
+    db_meta = await local_memory_store.get_metadata()
+    assert db_meta.to_dict() == local_meta
