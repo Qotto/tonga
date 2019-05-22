2b096681d534bbdaeba6870242fa7c9c69063f22
danglat <theo.dangla@epitech.eu>
2019-05-22 12:41:07 +0200

    Remove stores form services

--- aioevent/services/__init__.py
+++ aioevent/services/__init__.py
@@ -1,18 +1,3 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright (c) Qotto, 2019
-
-
-from .serializer import AvroSerializer, BaseSerializer
-from .consumer import AioeventConsumer, BaseConsumer
-from .producer import AioeventProducer, BaseProducer
-
-
-__all__ = [
-    'BaseSerializer',
-    'AvroSerializer',
-    'BaseProducer',
-    'AioeventProducer',
-    'BaseConsumer',
-    'AioeventConsumer',
-]
