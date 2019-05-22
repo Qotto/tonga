e0907c8b8e5a0d630e3d6edc343982820a3d347d
danglat <theo.dangla@epitech.eu>
2019-05-22 12:29:24 +0200

    Move event PyFile, remove handler function (handle)

--- aioevent/models/events/event/event.py
+++ aioevent/models/events/event/event.py
@@ -2,21 +2,15 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from aiokafka import TopicPartition
 
 from typing import Dict, Any
 
-from aioevent.app.base import BaseApp
-from aioevent.model import BaseModel
+from aioevent.models import BaseModel
 
 
 class BaseEvent(BaseModel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    # This method is called when the result is received
-    async def handle(self, app: BaseApp, corr_id: str, group_id: str, topic: TopicPartition, offset: int):
-        pass
 
     @classmethod
     def event_name(cls) -> str:
