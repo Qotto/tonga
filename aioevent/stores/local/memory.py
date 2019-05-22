2605993ecbd0dd8c56b85f4056e2740c019c9c13
danglat <theo.dangla@epitech.eu>
2019-05-22 12:45:18 +0200

    Add check_initialized decorator / add build_set() / build_delete() / set_initialized() function

--- aioevent/stores/local/memory.py
+++ aioevent/stores/local/memory.py
@@ -5,16 +5,17 @@
 import ast
 from aiokafka import TopicPartition
 
-from typing import Dict, Any, List
+from typing import Dict, Any, List, Union
 
-from aioevent.services.stores.local.base import BaseLocalStore
-from aioevent.services.stores import BaseStoreMetaData
-from aioevent.model.exceptions import StoreKeyNotFound, StoreMetadataCantNotUpdated
+from aioevent.stores.local.base import BaseLocalStore
+from aioevent.stores.base import BaseStoreMetaData
+from aioevent.models.exceptions import StoreKeyNotFound, StoreMetadataCantNotUpdated
+from aioevent.utils.decorator import check_initialized
 
 
 class LocalStoreMemory(BaseLocalStore):
     _db: Dict[str, bytes]
-    _store_metadata: BaseStoreMetaData
+    _store_metadata: Union[BaseStoreMetaData, None]
     _current_instance: int
     _nb_replica: int
 
@@ -23,25 +24,39 @@ class LocalStoreMemory(BaseLocalStore):
 
     _initialized: bool
 
-    def __init__(self, current_instance: int, nb_replica: int, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # DB as dict in StoreMemory
         self._db = dict()
-        self._current_instance = current_instance
-        self._nb_replica = nb_replica
-        self._initialized = False
 
-    def set_store_position(self, assigned_partitions: List[TopicPartition], last_offsets: Dict[TopicPartition, int]):
+        # Default param
+        self._current_instance = 0
+        self._nb_replica = 1
+        self._initialized = False
+        self._store_metadata = None
+
+        # Local store position
+        self._assigned_partitions = list()
+        self._last_offsets = dict()
+
+    def set_store_position(self, current_instance: int, nb_replica: int, assigned_partitions: List[TopicPartition],
+                           last_offsets: Dict[TopicPartition, int]) -> None:
         self._assigned_partitions = assigned_partitions
         self._last_offsets = last_offsets
+        self._nb_replica = nb_replica
+        self._current_instance = current_instance
 
         self._store_metadata = BaseStoreMetaData(self._assigned_partitions, self._last_offsets, self._current_instance,
                                                  self._nb_replica)
         self._update_metadata()
-        self._initialized = True
+
+    def set_initialized(self, initialized: bool) -> None:
+        self._initialized = initialized
 
     def is_initialized(self) -> bool:
         return self._initialized
 
+    @check_initialized
     def get(self, key: str) -> Any:
         if not isinstance(key, str):
             raise ValueError
@@ -49,6 +64,7 @@ class LocalStoreMemory(BaseLocalStore):
             raise StoreKeyNotFound(f'key: {key} was not found in LocalStoreMemory', 500)
         return self._db[key]
 
+    @check_initialized
     def set(self, key: str, value: bytes) -> None:
         if not isinstance(key, str) or not isinstance(value, bytes):
             raise ValueError
@@ -56,6 +72,7 @@ class LocalStoreMemory(BaseLocalStore):
             raise StoreMetadataCantNotUpdated(f'Fail to update metadata with set function', 500)
         self._db[key] = value
 
+    @check_initialized
     def delete(self, key: str) -> None:
         if not isinstance(key, str):
             raise ValueError
@@ -63,6 +80,21 @@ class LocalStoreMemory(BaseLocalStore):
             raise StoreKeyNotFound(f'Fail to delete key: {key}, not found in LocalStoreMemory', 500)
         del self._db[key]
 
+    def build_set(self, key: str, value: bytes) -> None:
+        if not isinstance(key, str) or not isinstance(value, bytes):
+            raise ValueError
+        if key == 'metadata':
+            raise StoreMetadataCantNotUpdated(f'Fail to update metadata with set function', 500)
+        self._db[key] = value
+
+    def build_delete(self, key: str) -> None:
+        if not isinstance(key, str):
+            raise ValueError
+        if key not in self._db:
+            raise StoreKeyNotFound(f'Fail to delete key: {key}, not found in LocalStoreMemory', 500)
+        del self._db[key]
+
+    @check_initialized
     def get_all(self) -> Dict[str, bytes]:
         return self._db
 
