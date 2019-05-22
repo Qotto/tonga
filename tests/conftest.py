0490612c3f1cab9e0031b2a1bc8823797fe9b8b6
danglat <theo.dangla@epitech.eu>
2019-05-22 16:11:18 +0200

    Load serializer class for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,23 @@
 # Copyright (c) Qotto, 2019
 
 import pytest
+import os
 
+# Serializer
+from aioevent.services.serializer.avro import AvroSerializer
+
+# Stores import
 from aioevent.stores.local.memory import LocalStoreMemory
 from aioevent.stores.globall.memory import GlobalStoreMemory
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 local_memory_store = LocalStoreMemory(name='local_store_memory_test')
 
 global_memory_store = GlobalStoreMemory(name='global_store_memory_test')
+
+serializer = AvroSerializer(BASE_DIR + '/misc/schemas')
 
 
 @pytest.fixture
@@ -21,3 +30,8 @@ def get_local_memory_store_connection():
 @pytest.fixture
 def get_global_memory_store_connection():
     return global_memory_store
+
+
+@pytest.fixture
+def get_avro_serializer():
+    return serializer
