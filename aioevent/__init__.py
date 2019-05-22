d0102e8cee595025714972afa0d28bd8a20c489e
danglat <theo.dangla@epitech.eu>
2019-05-22 12:47:41 +0200

    Update init PyFile

--- aioevent/__init__.py
+++ aioevent/__init__.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright (c) Qotto, 2019
-
-from aioevent.app.aioevent import AioEvent
-from aioevent.model.result.result import BaseResult
-from aioevent.model.command.command import BaseCommand
-from aioevent.model.event.event import BaseEvent
-
-__all__ = [
-    'AioEvent',
-    'BaseEvent',
-    'BaseResult',
-    'BaseCommand',
-]
