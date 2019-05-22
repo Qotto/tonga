08890e9aee822b9e2d5d406f21b98102fb908e6e
danglat <theo.dangla@epitech.eu>
2019-05-22 16:09:26 +0200

    Add async (store to async)

--- aioevent/models/store_record/store_record_handler.py
+++ aioevent/models/store_record/store_record_handler.py
@@ -4,8 +4,7 @@
 
 from aiokafka import TopicPartition
 
-from aioevent.models.store_record.base import BaseStoreRecordHandler
-from aioevent.models.store_record.store_record import StoreRecord
+from aioevent.models.store_record.base import BaseStoreRecordHandler, BaseStoreRecord
 from aioevent.stores.store_builder.base import BaseStoreBuilder
 from aioevent.models.exceptions import KtableUnknownType
 
@@ -24,7 +23,7 @@ class StoreRecordHandler(BaseStoreRecordHandler):
     def handler_name(cls) -> str:
         return 'aioevent.store.record'
 
-    async def local_store_handler(self, store_record: StoreRecord, group_id: str, tp: TopicPartition,
+    async def local_store_handler(self, store_record: BaseStoreRecord, group_id: str, tp: TopicPartition,
                                   offset: int) -> None:
         # Set or delete from local store
         if store_record.type == 'set':
@@ -34,16 +33,16 @@ class StoreRecordHandler(BaseStoreRecordHandler):
         else:
             raise KtableUnknownType(f'Local state type: {store_record.type} is unknown', 500)
         # Update metadata from local store
-        self._store_builder.update_metadata_from_local_store(tp, offset)
+        await self._store_builder.update_metadata_from_local_store(tp, offset)
 
-    async def global_store_handler(self, store_record: StoreRecord, group_id: str, tp: TopicPartition,
+    async def global_store_handler(self, store_record: BaseStoreRecord, group_id: str, tp: TopicPartition,
                                    offset: int) -> None:
         # Set or delete from global store
         if store_record.type == 'set':
-            self._store_builder.set_from_global_store(store_record.key, store_record.value)
+            await self._store_builder.set_from_global_store(store_record.key, store_record.value)
         elif store_record.type == 'del':
-            self._store_builder.delete_from_global_store(store_record.key)
+            await self._store_builder.delete_from_global_store(store_record.key)
         else:
             raise KtableUnknownType(f'Global state type: {store_record.type} is unknown', 500)
         # Update metadata from global store
-        self._store_builder.update_metadata_from_global_store(tp, offset)
+        await self._store_builder.update_metadata_from_global_store(tp, offset)
