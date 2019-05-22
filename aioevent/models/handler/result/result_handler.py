44017dd7d2dafe83fad6a2f4428258b4b4adbaf8
danglat <theo.dangla@epitech.eu>
2019-05-22 15:44:23 +0200

    Create BaseResultHandler inherit from BaseHandler

---
+++ aioevent/models/handler/result/result_handler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Qotto, 2019
+
+from aiokafka import TopicPartition
+
+from typing import Union
+
+from aioevent.models.handler.base import BaseHandler
+from aioevent.models.events.base import BaseModel
+
+__all__ = [
+    'BaseResultHandler',
+]
+
+
+class BaseResultHandler(BaseHandler):
+    @classmethod
+    def handler_name(cls) -> str:
+        raise NotImplementedError
+
+    async def on_result(self, event: BaseModel, tp: TopicPartition, group_id: str, offset: int) -> Union[str, None]:
+        raise NotImplementedError
