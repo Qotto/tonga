b5312210dac76ffc93564188e19d63641bc8b600
danglat <theo.dangla@epitech.eu>
2019-05-23 15:17:03 +0200

    Add event_loop fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@
 import os
 import pytest
 import asyncio
+import uvloop
 from kafka import KafkaAdminClient
 from kafka.cluster import ClusterMetadata
 
@@ -24,6 +25,7 @@ from aioevent.models.store_record.store_record_handler import StoreRecordHandler
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+t_loop = uvloop.new_event_loop()
 
 # Store test
 test_local_memory_store = LocalStoreMemory(name='local_store_memory_test')
@@ -39,14 +41,19 @@ store_builder_serializer = AvroSerializer(BASE_DIR + '/misc/schemas')
 
 admin_client = KafkaAdminClient(bootstrap_servers='localhost:9092', client_id='test_store_builder')
 cluster_metadata = ClusterMetadata(bootstrap_servers='localhost:9092')
-loop = asyncio.get_event_loop()
 test_store_builder_local_memory_store = LocalStoreMemory(name='test_store_builder_local_memory_store')
 test_store_builder_global_memory_store = GlobalStoreMemory(name='test_store_builder_global_memory_store')
 test_store_builder = StoreBuilder('test_store_builder', 0, 1, 'test-store', store_builder_serializer,
                                   test_store_builder_local_memory_store, test_store_builder_global_memory_store,
-                                  'localhost:9092', cluster_metadata, admin_client, loop, False, False)
+                                  'localhost:9092', cluster_metadata, admin_client, t_loop, False, False)
 store_record_handler = StoreRecordHandler(test_store_builder)
 store_builder_serializer.register_event_handler_store_record(StoreRecord, store_record_handler)
+
+
+@pytest.yield_fixture()
+def event_loop():
+    loop = t_loop
+    yield loop
 
 
 @pytest.fixture
