0867143adf03a2845ca83386daec394ec3f8d313
danglat <theo.dangla@epitech.eu>
2019-05-24 15:28:26 +0200

    Patched StatefulsetPartitioner tests (Instance changed)

--- tests/test_statefulset_partitioner.py
+++ tests/test_statefulset_partitioner.py
@@ -9,16 +9,14 @@ from aioevent.services.coordinator.partitioner.statefulset_partitioner import St
 
 
 def test_statefulset_partitioner_with_str_uuid_key():
-    statefulset_partitioner = StatefulsetPartitioner
-    setattr(statefulset_partitioner, 'instance', 1)
+    statefulset_partitioner = StatefulsetPartitioner(instance=1)
 
     for i in range(0, 100):
-        assert statefulset_partitioner.__call__(uuid.uuid4().hex, [0, 1, 2, 3], [0, 1, 2, 3]) == 1
+        assert statefulset_partitioner(uuid.uuid4().hex, [0, 1, 2, 3], [0, 1, 2, 3]) == 1
 
 
 def test_statefulset_partitioner_bad_instance():
-    statefulset_partitioner = StatefulsetPartitioner
-    setattr(statefulset_partitioner, 'instance', 100)
+    statefulset_partitioner = StatefulsetPartitioner(instance=100)
 
     with pytest.raises(ValueError):
         statefulset_partitioner.__call__(uuid.uuid4().hex, [0, 1, 2, 3], [0, 1, 2, 3])
