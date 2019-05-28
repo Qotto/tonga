fffebcf9cdd484f20d6e5dd14871ec5e6ad836ce
danglat <theo.dangla@epitech.eu>
2019-05-28 17:13:51 +0200

    Patched cash_register rest interface

--- examples/coffee_bar/cash_register/interfaces/rest/cash_register.py
+++ examples/coffee_bar/cash_register/interfaces/rest/cash_register.py
@@ -6,23 +6,37 @@ from sanic import Blueprint, response
 from sanic.request import Request
 from sanic.response import HTTPResponse
 
+from examples.coffee_bar.cash_register.models.bill import Bill
 
 cash_register_bp = Blueprint('Cash Register')
 
 
 @cash_register_bp.route('/cash-register', ['GET'])
 async def cash_register(request: Request) -> HTTPResponse:
-    in_cash, out_cash = request['aio_event'].get('cash_register_repository').how_mush_in_and_out()
+    local_store = request['cash_register']['store_builder'].get_local_store().get_all()
+    global_store = request['cash_register']['store_builder'].get_global_store().get_all()
+
+    local_cash_in = 0
+    local_cash_out = 0
+    for key, value in local_store.items():
+        bill = Bill.__from_bytes_dict__(value)
+        if bill.is_paid:
+            local_cash_in += bill.amount
+        else:
+            local_cash_out += bill.amount
+
+    global_cash_in = 0
+    global_cash_out = 0
+    for key, value in global_store.items():
+        bill = Bill.__from_bytes_dict__(value)
+        if bill.is_paid:
+            global_cash_in += bill.amount
+        else:
+            global_cash_out += bill.amount
+
     return response.json({
-        'amount_in': in_cash,
-        'amount_awaiting': out_cash
+        'local_cash_in': local_cash_in,
+        'local_cash_out': local_cash_out,
+        'global_cash_in': global_cash_in,
+        'global_cash_out': global_cash_out
     }, status=200)
-
-
-@cash_register_bp.route('/cash-register/full', ['GET'])
-async def cash_register(request: Request) -> HTTPResponse:
-    db = request['aio_event'].get('cash_register_repository').get_db()
-    l = list()
-    for uuid, bill in db.items():
-        l.append(bill.__dict__)
-    return response.json(l, status=200)
