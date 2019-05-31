52cf49e3f990a4ebb7a584e459b045b2f575d110
danglat <theo.dangla@epitech.eu>
2019-05-31 16:00:35 +0200

    Created two recipes bash script used by Makefile (repo manager)

---
+++ recipes/2019_05_31_dev_repo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Mv start example
+mv ./examples/coffee_bar/waiter/start_waiter.py .
+mv ./examples/coffee_bar/bartender/start_bartender.py .
+mv ./examples/coffee_bar/cash_register/start_cash_register.py .
+mv ./examples/coffee_bar/coffeemaker/start_coffeemaker.py .
+
+# Mv utils example
+mv ./examples/coffee_bar/start_coffee_bar_customers.py .
+
+# Mv docker-compose
+mv ./examples/dev_env/docker-compose.yml .
