077e9b17884433c4dff4ee22bbd2be44d971d8a6
danglat <theo.dangla@epitech.eu>
2019-05-31 12:11:27 +0200

    Created errors PyFile for easy dev loading

---
+++ aioevent/errors.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Qotto, 2019
+
+# Import StoreRecord exceptions
+from aioevent.models.store_record.errors import UnknownStoreRecordType
+
+# Import KeyPartitioner & StatefulsetPartitioner exceptions
+from aioevent.services.coordinator.partitioner.errors import (BadKeyType, OutsideInstanceNumber)
+
+# Import StatefulsetAssignors exceptions
+from aioevent.services.coordinator.assignors.errors import BadAssignorPolicy
+
+# Import Producer exceptions
+from aioevent.services.producer.errors import (ProducerConnectionError, AioKafkaProducerBadParams, KafkaProducerError,
+                                               KafkaProducerNotStartedError, KafkaProducerAlreadyStartedError,
+                                               KafkaProducerTimeoutError, KeyErrorSendEvent, ValueErrorSendEvent,
+                                               TypeErrorSendEvent, FailToSendEvent, UnknownEventBase, FailToSendBatch)
+
+# Import Consumer exceptions
+from aioevent.services.consumer.errors import (ConsumerConnectionError, AioKafkaConsumerBadParams, KafkaConsumerError,
+                                               KafkaConsumerNotStartedError, KafkaConsumerAlreadyStartedError,
+                                               ConsumerKafkaTimeoutError, IllegalOperation, TopicPartitionError,
+                                               NoPartitionAssigned, OffsetError, UnknownHandler,
+                                               UnknownStoreRecordHandler, UnknownHandlerReturn, HandlerException)
+
+# Import AvroSerializer & KeySerializer exceptions
+from aioevent.services.serializer.errors import (AvroAlreadyRegister, AvroEncodeError, AvroDecodeError, NotMatchedName,
+                                                 MissingEventClass, MissingHandlerClass, KeySerializerDecodeError,
+                                                 KeySerializerEncodeError)
+
+# Import LocalStore & GlobalStore exceptions
+from aioevent.stores.errors import (StoreKeyNotFound, StoreMetadataCantNotUpdated)
+
+# Import StoreBuilder exceptions
+from aioevent.stores.store_builder.errors import (UninitializedStore, CanNotInitializeStore, FailToSendStoreRecord)
+
+
+__all__ = [
+    # StoreRecord exceptions
+    'UnknownStoreRecordType',
+    # KeyPartitioner & StatefulsetPartitioner exceptions
+    'BadKeyType',
+    'OutsideInstanceNumber',
+    # StatefulsetAssignors exceptions
+    'BadAssignorPolicy',
+    # Producer exceptions
+    'ProducerConnectionError',
+    'AioKafkaProducerBadParams',
+    'KafkaProducerError',
+    'KafkaProducerNotStartedError',
+    'KafkaProducerAlreadyStartedError',
+    'KafkaProducerTimeoutError',
+    'KeyErrorSendEvent',
+    'ValueErrorSendEvent',
+    'TypeErrorSendEvent',
+    'FailToSendEvent',
+    'UnknownEventBase',
+    'FailToSendBatch',
+    # Consumer exceptions
+    'ConsumerConnectionError',
+    'AioKafkaConsumerBadParams',
+    'KafkaConsumerError',
+    'KafkaConsumerNotStartedError',
+    'KafkaConsumerAlreadyStartedError',
+    'ConsumerKafkaTimeoutError',
+    'IllegalOperation',
+    'TopicPartitionError',
+    'NoPartitionAssigned',
+    'OffsetError',
+    'UnknownHandler',
+    'UnknownStoreRecordHandler',
+    'UnknownHandlerReturn',
+    'HandlerException',
+    # AvroSerializer & KeySerializer exceptions
+    'AvroAlreadyRegister',
+    'AvroEncodeError',
+    'AvroDecodeError',
+    'NotMatchedName',
+    'MissingEventClass',
+    'MissingHandlerClass',
+    'KeySerializerDecodeError',
+    'KeySerializerEncodeError',
+    # LocalStore & GlobalStore exceptions
+    'StoreKeyNotFound',
+    'StoreMetadataCantNotUpdated',
+    # StoreBuilder exceptions
+    'UninitializedStore',
+    'CanNotInitializeStore',
+    'FailToSendStoreRecord'
+]
