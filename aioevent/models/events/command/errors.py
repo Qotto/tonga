c3439ca521733870ddf77bc61dc6085368714b19
danglat <theo.dangla@epitech.eu>
2019-05-31 12:59:21 +0200

    Patched missing Exception

--- aioevent/models/events/command/errors.py
+++ aioevent/models/events/command/errors.py
@@ -8,5 +8,5 @@ __all__ = [
 ]
 
 
-class CommandEventMissingProcessGuarantee:
+class CommandEventMissingProcessGuarantee(Exception):
     pass
--- aioevent/models/store_record/errors.py
+++ aioevent/models/store_record/errors.py
@@ -7,5 +7,5 @@ __all__ = [
 ]
 
 
-class UnknownStoreRecordType:
+class UnknownStoreRecordType(Exception):
     pass
