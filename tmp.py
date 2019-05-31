07cf7eececa2d51e8ce8bd194f5f4fb47a9420d3
danglat <theo.dangla@epitech.eu>
2019-05-31 10:39:28 +0200

    Updated tmp PyFile testing only (will by removed later)

--- tmp.py
+++ tmp.py
@@ -183,24 +183,26 @@ if __name__ == '__main__':
     server = sanic.create_server(host='0.0.0.0', port=sanic_port, debug=True, access_log=True,
                                  return_asyncio_server=True)
 
-    asyncio.ensure_future(server, loop=waiter_app['loop'])
+    waiter_producer = asyncio.ensure_future(waiter_app['consumer'].listen_event('committed'),
+                                            loop=waiter_app['loop'])
 
-    def my_exception_handler(loop, context):
-        print('Exception handler called')
+    sanic_server = asyncio.ensure_future(server, loop=waiter_app['loop'])
 
-        pprint(context)
-        print(type(context))
-        print(context)
-        if isinstance(context['exception'], ConsumerConnectionError):
-            loop.stop()
-
-    waiter_app['loop'].set_exception_handler(my_exception_handler)
+    async def start_app():
+        res = await asyncio.gather(waiter_producer, sanic_server, return_exceptions=True)
+        if isinstance(res[0], ConsumerConnectionError):
+            print('ConsumerConnectionError')
+            waiter_app['loop'].stop()
+        return res
 
     # Catch SIGINT
     signal(SIGINT, lambda s, f: waiter_app['loop'].stop())
     try:
         # Runs forever
-        waiter_app['loop'].run_forever()
+        waiter_app['loop'].run_until_complete(start_app())
+    except ConsumerConnectionError:
+        print('ConsumerConnectionError')
+        waiter_app['loop'].stop()
     except Exception:
         # If an exception was raised loop was stopped
         waiter_app['loop'].stop()
