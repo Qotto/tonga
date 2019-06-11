c4cc832202606bfb5589efc170b4a4fd9c32c3ec
danglat <theo.dangla@epitech.eu>
2019-06-11 09:54:42 +0200

    Added docstrings

--- tonga/stores/errors.py
+++ tonga/stores/errors.py
@@ -12,16 +12,32 @@ __all__ = [
 
 
 class StoreKeyNotFound(Exception):
+    """StoreKeyNotFound
+
+    This error was raised when store not found value by key
+    """
     pass
 
 
 class StoreMetadataCantNotUpdated(Exception):
+    """StoreMetadataCantNotUpdated
+
+    This error was raised when store can't update StoreMetadata
+    """
     pass
 
 
 class StorePartitionAlreadyAssigned(Exception):
+    """StorePartitionAlreadyAssigned
+
+    This error was raised when store is already assigned on topic
+    """
     pass
 
 
 class StorePartitionNotAssigned(Exception):
+    """StorePartitionNotAssigned
+
+    This error was raised when store have not assigned partition
+    """
     pass
