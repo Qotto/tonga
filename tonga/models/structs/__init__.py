d84cd4f9192af838ce805570ed2a9dbd14f39bad
danglat <theo.dangla@epitech.eu>
2019-06-17 12:19:38 +0200

    Created KafkaPositioning class

---
+++ tonga/models/structs/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright (c) Qotto, 2019
+
+from tonga.models.structs.kafka_positioning import KafkaPositioning
+from tonga.models.structs.store_record_type import StoreRecordType
+
+
+__all__ = [
+    'KafkaPositioning',
+    'StoreRecordType'
+]
