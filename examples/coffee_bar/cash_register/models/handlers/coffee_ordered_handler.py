06775c785f0095700b0707fb901e735faa557c8d
danglat <theo.dangla@epitech.eu>
2019-06-27 10:17:50 +0200

    Patched handler

--- examples/coffee_bar/cash_register/models/handlers/coffee_ordered_handler.py
+++ examples/coffee_bar/cash_register/models/handlers/coffee_ordered_handler.py
@@ -2,57 +2,47 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from aiokafka import TopicPartition
-
-from typing import Optional
-
-# Import BaseEvent
-from tonga.models.records.event import BaseEvent
 # Import BaseEventHandler
 from tonga.models.handlers.event.event_handler import BaseEventHandler
 # Import StoreBuilderBase
-from tonga.stores.store_builder.base import BaseStoreBuilder
+from tonga.stores.manager.kafka_store_manager import KafkaStoreManager
 # Import BaseProducer
 from tonga.services.producer.base import BaseProducer
+from tonga.services.coordinator.partitioner.key_partitioner import KeyPartitioner
 
 # Import Coffee Model
+from examples.coffee_bar.cash_register import transactional_manager
+from examples.coffee_bar.cash_register.models.events.coffee_ordered import CoffeeOrdered
 from examples.coffee_bar.cash_register.models.events.bill_created import BillCreated
 from examples.coffee_bar.cash_register.models.bill import Bill
 
 
 class CoffeeOrderedHandler(BaseEventHandler):
-    _store_builder: BaseStoreBuilder
+    _store_builder: KafkaStoreManager
     _transactional_producer: BaseProducer
 
-    def __init__(self, store_builder: BaseStoreBuilder, transactional_producer: BaseProducer, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, store_builder: KafkaStoreManager, transactional_producer: BaseProducer):
         self._store_builder = store_builder
         self._transactional_producer = transactional_producer
 
-    async def handle(self, event: BaseEvent, tp: TopicPartition, group_id: str, offset: int) -> Optional[str]:
-        if not self._transactional_producer.is_running():
-            await self._transactional_producer.start_producer()
+    @transactional_manager
+    async def handle(self, event: CoffeeOrdered) -> None:
 
-        async with self._transactional_producer.init_transaction():
-            # Creates commit_offsets dict
-            commit_offsets = {tp: offset + 1}
-            # Instances bill class
+        key_partitioner = KeyPartitioner()
+        while True:
             bill = Bill(coffee_uuid=event.uuid, amount=event.amount)
+            if key_partitioner(bill.uuid, [0, 1], [0, 1]) == self._store_builder.get_current_instance():
+                break
 
-            # Sets on cash register local store
-            await self._store_builder.set_from_local_store(bill.uuid, bill.__to_bytes_dict__())
+        # Sets on cash register local store
+        await self._store_builder.set_from_local_store(bill.uuid, bill.__to_bytes_dict__())
 
-            # Creates BillCreated event
-            bill_created = BillCreated(uuid=bill.uuid, coffee_uuid=bill.coffee_uuid, amount=bill.amount,
-                                       context=bill.context)
+        # Creates BillCreated event
+        bill_created = BillCreated(partition_key=bill.uuid, uuid=bill.uuid, coffee_uuid=bill.coffee_uuid,
+                                   amount=bill.amount, context=bill.context)
 
-            # Sends BillCreated event
-            await self._transactional_producer.send_and_await(bill_created, 'cash-register-events')
-
-            # End transaction
-            await self._transactional_producer.end_transaction(commit_offsets, group_id)
-        # TODO raise an exception
-        return 'transaction'
+        # Sends BillCreated event
+        await self._transactional_producer.send_and_wait(bill_created, 'cash-register-events')
 
     @classmethod
     def handler_name(cls) -> str:
--- examples/coffee_bar/cash_register/models/handlers/coffee_served_handler.py
+++ examples/coffee_bar/cash_register/models/handlers/coffee_served_handler.py
@@ -2,62 +2,45 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from aiokafka import TopicPartition
+from typing import Type
 
-from typing import Optional
-
-# Import BaseEvent
-from tonga.models.records.event import BaseEvent
 # Import BaseEventHandler
 from tonga.models.handlers.event.event_handler import BaseEventHandler
 # Import StoreBuilderBase
-from tonga.stores.store_builder.base import BaseStoreBuilder
+from tonga.stores.manager.kafka_store_manager import KafkaStoreManager
 # Import BaseProducer
 from tonga.services.producer.base import BaseProducer
 
 # Import Coffee Model
+from examples.coffee_bar.cash_register.models.events.coffee_served import CoffeeServed
 from examples.coffee_bar.cash_register.models.events.bill_paid import BillPaid
 from examples.coffee_bar.cash_register.models.bill import Bill
 
 
 class CoffeeServedHandler(BaseEventHandler):
-    _store_builder: BaseStoreBuilder
+    _store_builder: KafkaStoreManager
     _transactional_producer: BaseProducer
 
-    def __init__(self, store_builder: BaseStoreBuilder, transactional_producer: BaseProducer, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, store_builder: KafkaStoreManager, transactional_producer: BaseProducer):
         self._store_builder = store_builder
         self._transactional_producer = transactional_producer
 
-    async def handle(self, event: BaseEvent, tp: TopicPartition, group_id: str, offset: int) -> Optional[str]:
-        if not self._transactional_producer.is_running():
-            await self._transactional_producer.start_producer()
+    async def handle(self, event: Type[CoffeeServed]) -> None:
+        # Gets bill in local store
+        bill = Bill.__from_bytes_dict__(await self._store_builder.get_from_local_store(event.context['bill_uuid']))
 
-        async with self._transactional_producer.init_transaction():
-            # Creates commit_offsets dict
-            commit_offsets = {tp: offset + 1}
+        # Updates bill
+        bill.set_is_paid(True)
+        bill.set_context(event.context)
 
-            # Gets bill in local store
-            bill = Bill.__from_bytes_dict__(await self._store_builder.get_from_local_store(event.context['bill_uuid']))
+        # Sets updated bill on cash register local store
+        await self._store_builder.set_from_local_store(bill.uuid, bill.__to_bytes_dict__())
 
-            # Updates bill
-            bill.set_is_paid(True)
-            bill.set_context(event.context)
+        # Creates BillPaid event
+        bill_paid = BillPaid(uuid=bill.uuid, coffee_uuid=bill.coffee_uuid, amount=bill.amount, context=bill.context)
 
-            # Sets updated bill on cash register local store
-            await self._store_builder.set_from_local_store(bill.uuid, bill.__to_bytes_dict__())
-
-            # Creates BillPaid event
-            bill_paid = BillPaid(uuid=bill.uuid, coffee_uuid=bill.coffee_uuid, amount=bill.amount,
-                                 context=bill.context)
-
-            # Sends BillCreated event
-            await self._transactional_producer.send_and_await(bill_paid, 'cash-register-events')
-
-            # End transaction
-            await self._transactional_producer.end_transaction(commit_offsets, group_id)
-        # TODO raise an exception
-        return 'transaction'
+        # Sends BillCreated event
+        await self._transactional_producer.send_and_wait(bill_paid, 'cash-register-events')
 
     @classmethod
     def handler_name(cls) -> str:
