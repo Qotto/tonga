579f466c17887b43a5f04fb342bbecd31dd62759
danglat <theo.dangla@epitech.eu>
2019-06-27 10:30:12 +0200

    Removed TopicPartition dependencies / on_result now take only one param (BaseResult)

--- tonga/models/handlers/result/result_handler.py
+++ tonga/models/handlers/result/result_handler.py
@@ -9,8 +9,6 @@ All result handler must be inherit from this class. On_result function was calle
 For make an transaction in handle function return 'transaction' as string after end transaction otherwise return none.
 """
 
-from aiokafka import TopicPartition
-
 from typing import Union
 
 from tonga.models.handlers.base import BaseHandler
@@ -37,15 +35,11 @@ class BaseResultHandler(BaseHandler):
         """
         raise NotImplementedError
 
-    async def on_result(self, event: BaseResult, tp: TopicPartition, group_id: str, offset: int) -> Union[str, None]:
+    async def on_result(self, event: BaseResult) -> Union[str, None]:
         """ This function is automatically call by Tonga when an result with same name was receive by consumer
 
         Args:
             event (BaseResult): Result event receive by consumer
-            tp (TopicPartition): NamedTuple with topic name & partition number (more information in kafka-python
-                                 or aiokafka
-            group_id (str): Consumer group id, useful for make transaction in handler
-            offset (int): Offset of receive message (used for commit transaction)
 
         Raises:
             NotImplementedError: Abstract def
