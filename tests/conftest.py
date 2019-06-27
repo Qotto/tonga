39d2ff9328b6bd53a33963465bc3c51178c5d4c1
danglat <theo.dangla@epitech.eu>
2019-06-27 10:24:57 +0200

    Patched conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,18 +12,19 @@ from kafka.cluster import ClusterMetadata
 from tonga.services.serializer.avro import AvroSerializer
 
 # Stores import
-from tonga.stores.local.memory import LocalStoreMemory
-from tonga.stores.globall.memory import GlobalStoreMemory
+from tonga.stores.local_store.memory import LocalStoreMemory
+from tonga.stores.global_store.memory import GlobalStoreMemory
 
 # StoreBuilder import
-from tonga.stores.store_builder.store_builder import StoreBuilder
+from tonga.stores.manager.kafka_store_manager import KafkaStoreManager
 
 # StoreRecord import
-from tonga.models.records.store import StoreRecord
-from tonga.models.handlers.store.store_record_handler import StoreRecordHandler
+from tonga.models.store.store_record import StoreRecord
+from tonga.models.store.store_record_handler import StoreRecordHandler
+from tonga.stores.metadata.kafka_metadata import KafkaStoreMetaData
 
 # Tonga Kafka client
-from tonga.services.coordinator.kafka_client.kafka_client import KafkaClient
+from tonga.services.coordinator.client.kafka_client import KafkaClient
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -48,15 +49,20 @@ store_builder_serializer = AvroSerializer(BASE_DIR + '/misc/schemas')
 
 tonga_kafka_client = KafkaClient(client_id='waiter', cur_instance=0, nb_replica=1, bootstrap_servers='localhost:9092')
 
-test_store_builder_local_memory_store = LocalStoreMemory(name='test_store_builder_local_memory_store')
-test_store_builder_global_memory_store = GlobalStoreMemory(name='test_store_builder_global_memory_store')
+test_store_manager_local_memory_store = LocalStoreMemory(name='test_store_builder_local_memory_store')
+test_store_manager_global_memory_store = GlobalStoreMemory(name='test_store_builder_global_memory_store')
 
-test_store_builder = StoreBuilder(name='test_store_builder', client=tonga_kafka_client, topic_store='test-store',
-                                  serializer=store_builder_serializer, loop=t_loop, rebuild=True, event_sourcing=False,
-                                  local_store=test_store_builder_local_memory_store,
-                                  global_store=test_store_builder_global_memory_store)
+test_local_memory_store.set_metadata_class(KafkaStoreMetaData)
+test_global_memory_store.set_metadata_class(KafkaStoreMetaData)
 
-store_record_handler = StoreRecordHandler(test_store_builder)
+test_store_manager = KafkaStoreManager(name='test_store_builder', client=tonga_kafka_client, topic_store='test-store',
+                                       serializer=store_builder_serializer, loop=t_loop, rebuild=True,
+                                       event_sourcing=False,
+                                       local_store=test_store_manager_local_memory_store,
+                                       global_store=test_store_manager_global_memory_store)
+
+
+store_record_handler = StoreRecordHandler(test_store_manager)
 store_builder_serializer.register_event_handler_store_record(StoreRecord, store_record_handler)
 
 
@@ -97,5 +103,5 @@ def get_assignor_cluster_metadata():
 
 
 @pytest.fixture
-def get_store_builder():
-    return test_store_builder
+def get_store_manager():
+    return test_store_manager
