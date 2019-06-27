f1c37f43adfd1aa2383739273ec3f931d49c5876
danglat <theo.dangla@epitech.eu>
2019-06-27 10:36:52 +0200

    Created StoreRecordType

--- tonga/models/structs/store_record_type.py
+++ tonga/models/structs/store_record_type.py
@@ -2,6 +2,10 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
+"""
+Contains StoreRecordType class, used by StoreBuilder for set operation_type (StoreRecord)
+"""
+
 from enum import Enum
 
 
@@ -11,9 +15,13 @@ __all__ = [
 
 
 class StoreRecordType(Enum):
-    """ KafkaPositioning class
+    """ StoreRecordType
 
-    Class manage kafka positioning (like TopicsPartition)
+    Used by StoreBuilder as *operating_type* for *set* or *del* key & value in store
+
+    Attributes:
+        SET (str): StoreRecord operation type (created or updated)
+        DEL (str): StoreRecord operation type (deleted)
     """
-    SET: int = 0
-    DEL: int = 1
+    SET: str = 'set'
+    DEL: str = 'del'
