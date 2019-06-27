9c2fee673bdf9c156349706c5fced49830404c4d
danglat <theo.dangla@epitech.eu>
2019-06-27 10:16:56 +0200

    Added to_dict & from_dict method in record class

--- examples/coffee_bar/cash_register/models/events/bill_created.py
+++ examples/coffee_bar/cash_register/models/events/bill_created.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
+from datetime import datetime, timezone
 from tonga.models.records.event import BaseEvent
 
 from typing import Dict, Any
@@ -23,9 +24,24 @@ class BillCreated(BaseEvent):
         self.coffee_uuid = coffee_uuid
         self.amount = amount
 
+    def to_dict(self) -> Dict[str, Any]:
+        r_dict = self.base_dict()
+        r_dict['uuid'] = self.uuid
+        r_dict['coffee_uuid'] = self.coffee_uuid
+        r_dict['amount'] = self.amount
+        return r_dict
+
     @classmethod
-    def from_data(cls, event_data: Dict[str, Any]):
-        return cls(**event_data)
+    def from_dict(cls, dict_data: Dict[str, Any]):
+        return cls(schema_version=dict_data['schema_version'],
+                   record_id=dict_data['record_id'],
+                   partition_key=dict_data['partition_key'],
+                   date=datetime.fromtimestamp(dict_data['timestamp'] / 1000, timezone.utc),
+                   correlation_id=dict_data['correlation_id'],
+                   context=dict_data['context'],
+                   uuid=dict_data['uuid'],
+                   coffee_uuid=dict_data['coffee_uuid'],
+                   amount=dict_data['amount'])
 
     @classmethod
     def event_name(cls) -> str:
