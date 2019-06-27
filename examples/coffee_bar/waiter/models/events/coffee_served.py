ab61a0df9defab3a2a80ac17ca13b3ef5ac6797c
danglat <theo.dangla@epitech.eu>
2019-06-27 10:20:59 +0200

    Added to_dict & from_dict method in waiter records

--- examples/coffee_bar/waiter/models/events/coffee_served.py
+++ examples/coffee_bar/waiter/models/events/coffee_served.py
@@ -2,9 +2,11 @@
 # coding: utf-8
 # Copyright (c) Qotto, 2019
 
-from tonga.models.records.event import BaseEvent
+from datetime import datetime, timezone
 
 from typing import Dict, Any
+
+from tonga.models.records.event import BaseEvent
 
 
 __all__ = [
@@ -25,9 +27,26 @@ class CoffeeServed(BaseEvent):
         self.is_payed = is_payed
         self.amount = amount
 
+    def to_dict(self) -> Dict[str, Any]:
+        r_dict = self.base_dict()
+        r_dict['uuid'] = self.uuid
+        r_dict['served_to'] = self.served_to
+        r_dict['is_payed'] = self.is_payed
+        r_dict['amount'] = self.amount
+        return r_dict
+
     @classmethod
-    def from_data(cls, event_data: Dict[str, Any]):
-        return cls(**event_data)
+    def from_dict(cls, dict_data: Dict[str, Any]):
+        return cls(schema_version=dict_data['schema_version'],
+                   record_id=dict_data['record_id'],
+                   partition_key=dict_data['partition_key'],
+                   date=datetime.fromtimestamp(dict_data['timestamp'] / 1000, timezone.utc),
+                   correlation_id=dict_data['correlation_id'],
+                   context=dict_data['context'],
+                   uuid=dict_data['uuid'],
+                   served_to=dict_data['served_to'],
+                   is_payed=dict_data['is_payed'],
+                   amount=dict_data['amount'])
 
     @classmethod
     def event_name(cls) -> str:
