ca724142b1ab1d880a0bfce608ddf1cc4166f188
danglat <theo.dangla@epitech.eu>
2019-06-27 10:55:53 +0200

    Updated init

--- tonga/__init__.py
+++ tonga/__init__.py
@@ -9,43 +9,27 @@ Import tonga component
 
 __version__ = '0.0.1'
 
-# Import KafkaConsumer / KafkaProducer
-from .services.consumer.kafka_consumer import KafkaConsumer
-from .services.producer.kafka_producer import KafkaProducer
-
-# Import AvroSerializer
-from .services.serializer.avro import AvroSerializer
-
-# Import KeyPartitioner
-from .services.coordinator.partitioner.key_partitioner import KeyPartitioner
-
-# Import BaseEvent / BaseCommand / BaseResult
-from .models.records.base import BaseRecord
-from .models.records.event.event import BaseEvent
-from .models.records.command.command import BaseCommand
-from .models.records.result.result import BaseResult
-
-# Import BaseEventHandler / BaseCommandHandler / BaseResultHandler
-from .models.handlers.event.event_handler import BaseEventHandler
+from tonga.models.store.store_record import StoreRecord
+from tonga.models.store.store_record_handler import StoreRecordHandler
 from .models.handlers.command.command_handler import BaseCommandHandler
+from .models.handlers.event.event_handler import BaseEventHandler
 from .models.handlers.result.result_handler import BaseResultHandler
-
-# Import StoreRecord / StoreRecordHandler
-from .models.records.store.store_record import StoreRecord
-from tonga.models.handlers.store.store_record_handler import StoreRecordHandler
-
-# Import StoreBuilder
-from .stores.store_builder.store_builder import StoreBuilder
-
-# Import LocalStoreMemory / GlobalStoreMemory
-from .stores.local.memory import LocalStoreMemory
-from .stores.globall.memory import GlobalStoreMemory
-
+from .models.records.base import BaseRecord
+from .models.records.command.command import BaseCommand
+from .models.records.event.event import BaseEvent
+from .models.records.result.result import BaseResult
+from .services.consumer.kafka_consumer import KafkaConsumer
+from .services.coordinator.partitioner.key_partitioner import KeyPartitioner
+from .services.producer.kafka_producer import KafkaProducer
+from .services.serializer.avro import AvroSerializer
+from .stores.global_store.memory import GlobalStoreMemory
+from .stores.local_store.memory import LocalStoreMemory
 
 __all__ = [
     # KafkaConsumer / KafkaProducer
     'KafkaConsumer',
     'KafkaProducer',
+
     # AvroSerializer
     'AvroSerializer',
     # KeyPartitioner
@@ -62,8 +46,6 @@ __all__ = [
     # StoreRecord / StoreRecordHandler
     'StoreRecord',
     'StoreRecordHandler',
-    # StoreBuilder
-    'StoreBuilder',
     # LocalStoreMemory / GlobalStoreMemory
     'LocalStoreMemory',
     'GlobalStoreMemory'
